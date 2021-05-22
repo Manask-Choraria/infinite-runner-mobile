f16cc66814fcdc66ce608cc033886dfc3c0a3689
Manask-Choraria <71078042+Manask-Choraria@users.noreply.github.com>
2021-05-22 17:48:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# p5.play-boilerplate
+This is a infinite runner game by Manask Choraria. In this game you have to dodge the obstacles and earn the coins to increase the coin score. It is a pretty challanging game because the speed will keep increasing as you proceed in this game, which is fun. If you fail to dodge the obstacle the game will get over, you will be able to see the coin score and distance covered at the top right corner. There is a reset button, by clicking it the game will start from the beginning. This game is compatible with android.This game has fluent and easy controls which will give you a nice experience.  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sketch.js
@@ -0,0 +1,169 @@
+var player,ground,coinsGroup,obstaclesGroup,obstacleImg,coinsImg,groundImg,playerImg,coinScore,gameOver
+gameState,gameOverimg,reset,resetImg,coinSound,gameOverSound,resetTap,obstacleTouchingSound;
+var playerStop
+var score = 0;
+function preload(){
+  obstacleImg = loadImage("Images/Pad_01_2.png")
+  coinsImg = loadImage("Images/Coin_03.png")
+  groundImg = loadAnimation("Images/road5.jpg")
+  playerImg = loadAnimation("Images/player1.png","Images/player2.png","Images/player3.png",
+  "Images/player4.png","Images/player5.png","Images/player6.png")
+  playerStop = loadImage("Images/player4.png") 
+  gameOverimg = loadImage("Images/gameover.png")
+  resetImg = loadImage("Images/reset.png")
+  coinSound = loadSound("Images/coinbonus.wav");
+  gameOverSound = loadSound("Images/gameOver.wav")
+  resetTap = loadSound("Images/mouseclick.wav")
+  obstacleTouchsound = loadSound("Images/obstacletouching.wav")
+
+
+
+
+       
+
+
+
+
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  gameState = "play";
+ ground = createSprite(windowWidth/2,windowHeight/2);
+ ground.scale =1.5;
+ ground.addAnimation("r",groundImg);
+ player = createSprite(windowWidth/2, windowHeight-100, 50, 50);
+ player.addAnimation("run",playerImg)
+ gameOver = createSprite(windowWidth/2,windowHeight/2-100);
+ gameOver.visible = false;
+ gameOver.addImage(gameOverimg)
+ gameOver.scale = 0.5
+ reset = createSprite(windowWidth/2,windowHeight/2+50);
+ reset.addImage(resetImg);
+ reset.visible = false;
+ reset.scale = 0.3;
+ coinsGroup = new Group()
+ obstaclesGroup = new Group();
+ coinScore = 0;
+ score = 0;
+
+}
+
+function draw() {
+  background(0);
+  if(gameState === "play"){
+  if(ground.y>450){
+    ground.y = 200;
+  }
+  score = score + Math.round(getFrameRate()/60);
+  ground.velocityY = 10+score/100;
+  generateCoins();
+  createObstacles();
+  if(coinsGroup.isTouching(player)){
+    coinScore += 1
+    coinsGroup.destroyEach();
+    coinSound.play();
+    
+   }
+  
+   
+   
+  if(obstaclesGroup.isTouching(player)){
+   gameState = "end";
+   obstacleTouchsound.play();
+  }}
+  if(gameState === "end"){
+    gameOver.visible = true
+    player.addAnimation("stop",playerStop)
+    player.changeAnimation("stop",playerStop);
+    ground.velocityY = 0;
+    coinsGroup.destroyEach();
+    obstaclesGroup.destroyEach();
+    reset.visible = true;
+      
+    if(mousePressedOver(reset)|| touches.length>0){
+      gameReset();
+      resetTap.play();
+  
+    }
+
+    
+  }
+
+  
+  
+
+
+  
+ 
+
+  drawSprites();
+  fill("aqua");
+  text("COINS SCORE: " +coinScore,windowWidth/2+140,110);
+  text("DISTANCE: " +score,windowWidth/2+140,150);
+
+}
+function gameReset(){
+  gameState = "play";
+  gameOver.visible = false;
+  reset.visible = false;
+  obstaclesGroup.destroyEach();
+  coinsGroup.destroyEach();
+  player.changeAnimation("run",playerImg);
+  coinScore = 0;
+  ground.velocityY = 10;
+  score = 0;    
+  
+}
+
+function keyPressed(){
+  if((keyCode ===RIGHT_ARROW||touches[x]===300||touches[x]===450) && player.x<windowWidth/2+150){
+    player.x += 150;
+
+  }
+  else if((keyCode ===LEFT_ARROW||touches[x]===300||touches[x]===150) && player.x>windowWidth/2-150){
+    player.x -= 150;
+  }
+}   
+
+//coins will be generated through this  function
+function generateCoins(){
+    if(frameCount%60 === 0){
+       coins = createSprite(50,0,10,10);
+       coins.addImage(coinsImg);
+       var number = Math.round(random(1,3))
+       switch(number){
+         case 1:coins.x = windowWidth/2-150
+         break;
+         case 2:coins.x = windowWidth/2
+         break;
+         case 3:coins.x = windowWidth/2+150
+         break;
+       }
+       coins.velocityY = 20+score/100;
+       coins.lifetime = 350;
+       coins.scale = 0.1;
+       coinsGroup.add(coins)
+    
+    }
+}
+
+function createObstacles(){
+  if(frameCount%95 === 0){
+    obstacles = createSprite(50,0,20,10);
+    obstacles.addImage(obstacleImg);
+    var number = Math.round(random(1,3))
+    switch(number){
+      case 1:obstacles.x = windowWidth/2-150;
+      break;
+      case 2:obstacles.x = windowWidth/2;
+      break;
+      case 3:obstacles.x = windowWidth/2+150;
+      break;
+    }
+    obstacles.velocityY = 20+score/100;
+    obstacles.lifetime = 350;
+    obstacles.scale = 0.1;
+    obstaclesGroup.add(obstacles);
+  }
+}
